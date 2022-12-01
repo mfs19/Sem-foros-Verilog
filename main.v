3b1aa7268157f3388a2361033eb07319a79e9ccf
mfs19 <114117535+mfs19@users.noreply.github.com>
2022-12-01 20:27:59 -0300

    Update main.v

--- main.v
+++ main.v
@@ -12,7 +12,6 @@ module Traffic_Light_Controller(
     parameter  S1=0, S2=1, S3 =2, S4=3, S5=4,S6=5;
     reg [3:0]count;
     reg[2:0] ps;
-    parameter  sec7=7,sec5=5,sec2=2,sec3=3;
 
    
     
@@ -29,7 +28,7 @@ module Traffic_Light_Controller(
        
             
             case(ps)
-                S1: if(count<sec7)
+                S1: if(count<7)
                         begin
                         ps<=S1;
                         count<=count+1;
@@ -39,7 +38,7 @@ module Traffic_Light_Controller(
                         ps<=S2;
                         count<=0;
                         end
-                S2: if(count<sec2)
+                S2: if(count<2)
                         begin
                         ps<=S2;
                         count<=count+1;
@@ -50,7 +49,7 @@ module Traffic_Light_Controller(
                         ps<=S3;
                         count<=0;
                         end
-                S3: if(count<sec5)
+                S3: if(count<5)
                         begin
                         ps<=S3;
                         count<=count+1;
@@ -61,7 +60,7 @@ module Traffic_Light_Controller(
                         ps<=S4;
                         count<=0;
                         end
-                S4:if(count<sec2)
+                S4:if(count<2)
                         begin
                         ps<=S4;
                         count<=count+1;
@@ -72,7 +71,7 @@ module Traffic_Light_Controller(
                         ps<=S5;
                         count<=0;
                         end
-                S5:if(count<sec3)
+                S5:if(count<3)
                         begin
                         ps<=S5;
                         count<=count+1;
@@ -84,7 +83,7 @@ module Traffic_Light_Controller(
                         count<=0;
                         end
 
-                S6:if(count<sec2)
+                S6:if(count<2)
                         begin
                         ps<=S6;
                         count<=count+1;
