6f68a499eec42ce5699ece2332908b44b0f85a5f
mfs19 <114117535+mfs19@users.noreply.github.com>
2022-12-01 23:32:45 -0300

    Update semaforo.v

--- semaforo.v
+++ semaforo.v
@@ -1,8 +1,8 @@
 module ControladorSemaforo(
   input clk, reset,
   input Sa, Sb //sensores de carros
-  output reg Va, Aa, Vea //Va = vermelho para rua a, Aa = amarelo para rua a, Vea = verde para rua a
-  output reg Vb, Ab, Veb // similar ao anterior
+  output reg Va, Aa, Vea, Vap, Aap, Veap; //Va = vermelho para rua a, Aa = amarelo para rua a, Vea = verde para rua a
+  output reg Vb, Ab, Veb, Vbp, Abp, Vebp; // similar ao anterior
 );
   
   reg [3:0] state_reg, state_next;
@@ -46,26 +46,41 @@ module ControladorSemaforo(
       Vb = 1'b0;
       Ab = 1'b0;
       Veb = 1'b0;
+      Vap = 1'b0;
+      Aap = 1'b0;
+      Veap = 1'b0;
+      Vbp = 1'b0;
+      Abp = 1'b0;
+      Vebp = 1'b0;
+      
       case(state_reg)
         s0,s1,s2:
           begin
             Vea = 1'b1;
+            Vap = 1'b1;
             Vb = 1'b1;
+            Vebp = 1'b1;
           end
         s3:
           begin
             Aa = 1'b1;
+            Vap = 1'b1;
             Vb = 1'b1;
+            Abp = 1'b1;
           end
         s4,s5,s6:
           begin
             Va = 1'b1;
+            Aap = 1'b1;
             Ab = 1'b1;
+            Vebp = 1'b1;
           end
         s7:
           begin
             Va = 1'b1;
+            Veap = 1'b1;
             Ab = 1'b1;
+            Vebp = 1'b1;
           end
       endcase
     end
